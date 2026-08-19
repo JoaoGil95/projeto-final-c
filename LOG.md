3e2dfa1e24a5ad90b190171deb75f7340d470e5b
Joao Gil <joaopcgil@proton.me>
2026-08-19 13:56:25 +0100

    Sessão 2 - 19/08/2026

--- LOG.md
+++ LOG.md
@@ -19,3 +19,27 @@ Testar o funcionamento do git.
 
 **Linhas de código escritas hoje (estimativa):**
 - Cerca de 20
+
+## Sessão 2 - 19/08/2026
+**Funcionalidades implementadas hoje:**
+- Todas as opções do menu principal em funcionamento;
+- Todas as opções das melhorias escritas;
+- Testes de robustez do jogo (verificação de loops infinitos, números sem lógica, funções mal implementadas, etc.)
+
+**Maior dificuldade encontrada e como resolvi (ou não resolvi):**
+- Integrar as várias funções e variáveis em ficheiros .c ou .h -> Não implementado
+- Usar #define ou constantes -> não implementado
+- Perceber quais as variáveis e funções que necessito -> resolvido (a não ser que necessite de mais funções entretanto)
+- Eventos "Tempestade", "Furo depósito" e "Seca" -> não implementado
+- Dinâmica do jogo; perceber as condições iniciais, probabilidades dos eventos e custos/regenerações/consumos mais adequados para tornar o jogo equilibrado (nem demasiado dificil, nem demasiado fácil) -> parcialmente resolvido
+- Cálculo da pontuação do jogo -> não implementado
+- Cores dos hectares do terreno -> não implementado (não é prioritário)
+
+**Próximo passo planeado:**
+- Terminar eventos;
+- Calcular pontuação do jogo;
+- Mais testes de robustez e dinâmica do jogo;
+- Começar a condesar e organizar funções em ficheiros .c e .h
+
+**Linhas de código escritas hoje (estimativa):**
+- 250-300
--- game.c
+++ game.c
@@ -1,30 +1,64 @@
 #include <stdio.h>
-#include <unistd.h> //para sleep()
+#include <unistd.h>  //para sleep()
+#include <stdbool.h> //booleans
+#include <stdlib.h>  // rand() e srand()
+#include <time.h>    // time()
 
 //---variáveis---//
 int opcao_menu;     // opção menu principal
 int opcao_melhoria; // opção das melhorias
 int turno;          // turno do jogo
 char quinta[3][3];  // a quinta
-int sementes;       // sementes
-int agua;           // água na quinta
-int fertilizante;   // sacos de fertilizante
+int evento;         // escolher o evento/intempérie
+
+int contador_culturas; // para contar as culturas no terreno
+int sementes;          // sementes na quinta
+int regen_sementes;    // regeneração de sementes pelas culturas
+
+int agua;            // água na quinta
+int capacidade_agua; // reservatório de água
+int regen_agua;      // regeneração de água
+int consumo_agua;    // consumo de água das culturas
+
+int fertilizante; // sacos de fertilizante
+int comida;       // unidades de comida
+int regen_comida; // regeneração de comida
+bool divino;      // proteção divina
+
+// variáveis de produção com define
+// define chuva - regeneração natural
+// define os vários custos das melhorias
+// meter define das condições iniciais?
 
 //---listagem das funções---//
 // funções recorrentes do jogo
 void iniciar_quinta(char quinta[3][3]);
 void print_quinta(char quinta[3][3]);
-// void specs_quinta;
+void specs_quinta(void);
+void analisar_quinta(char quinta[3][3]);
+void crescer_sementes(char quinta[3][3]);
 
 // funções do menu (ações do jogo)
-// void plantar_semente;
-// void usar_fertilizante;
-// void melhorias;
-// void evento_intemperie;
+void plantar_semente(char quinta[3][3]); // planta uma semente
+void usar_fertilizante();
+void melhorias(void);
+void recursos_quinta(void); // faz todas as ações necessárias
+
+// funções aleatórias do jogo (eventos e intempéries)
+int evento_intemperie(void);
+
+// void seca(char quinta[3][3]);
+// void tempestade(char quinta[3][3]);
+void passaros(void);
+// void furo_deposito(void);
+
+// função de finalização do jogo
+//  void fim_do_jogo; //calcula pontuação
 
 //***---O JOGO---***//
 int main(void)
 {
+    srand(time(NULL));
     /*
     usleep(500000);
     printf("Com que então decidiste jogar a Quinta da Sobrevivência...\n");
@@ -74,48 +108,101 @@ int main(void)
     printf("jgs      ##V##         ##V##\n");
     usleep(500000); */
 
-    // imprimir a quinta
+    // iniciar a quinta
     iniciar_quinta(quinta);
-    print_quinta(quinta);
+    analisar_quinta(quinta);
 
-    // mostrar menu
-    printf("\nO que vais fazer agora?\n");
-    printf("1. Plantar Semente\n");
-    printf("2. Usar fertilizante\n");
-    printf("3. Melhorias\n");
-    printf("4. Processar turno\n");
-    printf("0. Sair\n");
-
-    scanf("%d", &opcao_menu);
-    getchar();
-
-    switch (opcao_menu)
+    while (1) // loop do jogo
     {
-    case 1:
-        /* code */
-        break;
+        // mostra a quinta
+        specs_quinta();
+        print_quinta(quinta);
 
-    case 2:
-        /* code */
-        break;
+        // mostrar menu
+        printf("\nO que vais fazer agora?\n");
+        printf("1. Plantar Semente\n");
+        printf("2. Usar fertilizante\n");
+        printf("3. Melhorias\n");
+        printf("4. Processar turno\n");
+        printf("0. Sair\n");
 
-    case 3:
-        /* code */
-        break;
+        scanf("%d", &opcao_menu);
+        getchar();
 
-    case 4:
-        break;
+        switch (opcao_menu)
+        {
+        case 1: // plantar semente
+            plantar_semente(quinta);
+            break;
 
-    case 0:
-        printf("A tua quinta foi abandonada.\n");
-        usleep(2000000);
-        printf("Parece que ser agricultor não é nada fácil...\n");
-        usleep(2000000);
-        break;
+        case 2: // usar fertilizante
+            usar_fertilizante();
+            break;
 
-    default:
-        printf("Opção inválida.\n");
-        break;
+        case 3:
+            melhorias();
+            break;
+
+        case 4:
+            analisar_quinta(quinta);
+            recursos_quinta();
+            // eventos e intempéries
+            int evento = evento_intemperie();
+            if (divino == true)
+            {
+                evento = 0;
+                printf("Safaste-te da intempérie.\n");
+                divino = false;
+            }
+            else
+            {
+                switch (evento)
+                {
+                case 1:
+                    // tempestade
+                    break;
+
+                case 2:
+                    // seca
+                    break;
+
+                case 3:
+                    passaros();
+                    break;
+
+                case 4:
+                    // furo depósito
+                    break;
+
+                default:
+                    break;
+                }
+            }
+
+            // verificar condições de fim de jogo
+            if (agua <= 0)
+            {
+                printf("FICASTE SEM ÁGUA!!! FIM DO JOGO\n");
+                // calcula pontuação
+                return 0;
+            }
+            // crescer sementes
+            crescer_sementes(quinta);
+            analisar_quinta(quinta);
+            turno++;
+            break;
+
+        case 0:
+            usleep(1000000);
+            printf("A tua quinta foi abandonada. Parece que ser agricultor não é nada fácil...\n");
+            usleep(1000000);
+            printf("\n");
+            return 0;
+
+        default:
+            printf("Opção inválida.\n");
+            break;
+        }
     }
 
     return 0;
@@ -131,7 +218,25 @@ void iniciar_quinta(char quinta[3][3])
             quinta[i][j] = '.';
         }
     }
-    quinta[0][0] = 'C'; // começa com 1 cultura
+    quinta[0][0] = 'C';
+    sementes = 10;
+    agua = 100000;
+    capacidade_agua = 200000000;
+    fertilizante = 1;
+    comida = 2;
+    regen_agua = 1; // lençóis freáticos
+    turno = 1;
+}
+
+void specs_quinta(void)
+{
+    printf("Turno: %d\n", turno);
+    printf("=== RECURSOS ===\n");
+    printf("Sementes: %d | Água: %d/%d | Fertilizante: %d | Comida: %d\n",
+           sementes, agua, capacidade_agua, fertilizante, comida);
+    printf("================\n");
+    printf("Prod. água: %d | Prod. comida: %d | Prod. sementes: %d\n",
+           regen_agua - consumo_agua, regen_comida, regen_sementes);
 }
 
 void print_quinta(char quinta[3][3])
@@ -145,5 +250,193 @@ void print_quinta(char quinta[3][3])
         }
         printf("\n");
     }
-    printf("\n");
+}
+
+void plantar_semente(char quinta[3][3])
+{
+    if (sementes <= 0)
+    {
+        printf("Sem sementes.\n");
+        return;
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (quinta[i][j] == '.')
+            {
+                quinta[i][j] = 's';
+                sementes--;
+                agua--;
+                return; // Sai da função logo após plantar a primeira
+            }
+        }
+    }
+    printf("Não tens hectares disponíveis.\n");
+}
+
+void usar_fertilizante(void)
+{
+    if (fertilizante > 0)
+    {
+        regen_comida++;
+        fertilizante--;
+        return;
+    }
+    printf("Sem fertilizante.\n");
+}
+
+void melhorias(void)
+{
+    while (1)
+    {
+        // mostrar menu
+        printf("\nMelhoria:\n");
+        printf("1. Regadio: +2 regen. água. Custa 2 comidas.\n");
+        printf("2. Depósito melhorado: +10 capacidade de água. Custa 4 comidas.\n");
+        printf("3. Saco fertilizante. +1 regen. comida. Custa 1 comida.\n");
+        printf("4. Proteção divina. Protege-te de qualquer intempérie num turno. Custa 10 comidas.\n");
+        printf("0. Voltar à quinta\n");
+
+        scanf("%d", &opcao_melhoria);
+        getchar();
+
+        switch (opcao_melhoria)
+        {
+        case 1:             // regadio
+            if (comida < 2) // depois tenho de alterar isto para um custo fixo do regadio
+            {
+                printf("Não tens comida suficiente.\n");
+                break;
+            }
+            regen_agua += 2;
+            comida -= 2;
+            break;
+
+        case 2:             // depósito
+            if (comida < 4) // depois tenho de alterar isto para um custo fixo do depósito
+            {
+                printf("Não tens comida suficiente.\n");
+                break;
+            }
+            capacidade_agua += 10;
+            comida -= 4;
+            break;
+
+        case 3:             // saco fertilizante
+            if (comida < 1) // depois tenho de alterar isto para um custo fixo do fertilizante
+            {
+                printf("Não tens comida suficiente.\n");
+                break;
+            }
+            fertilizante++;
+            comida--;
+            break;
+
+        case 4: // proteção divina
+            if (divino == false && comida >= 10)
+            {
+                divino = true;
+                break;
+            }
+            else if (comida < 10) // depois tenho de alterar isto para um custo fixo do fertilizante
+            {
+                printf("Não tens comida suficiente.\n");
+                break;
+            }
+            else if (divino == true) // depois tenho de alterar isto para um custo fixo do fertilizante
+            {
+                printf("Já estás protegido.\n");
+                break;
+            }
+            break;
+
+        case 0:
+            return;
+
+        default:
+            printf("Opção inválida.\n");
+            break;
+        }
+    }
+}
+
+void analisar_quinta(char quinta[3][3]) // ver o que têm os hectares de terreno
+{
+    // analisa a quinta
+    contador_culturas = 0;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (quinta[i][j] == 'C')
+            {
+                contador_culturas++;
+            }
+        }
+    }
+    consumo_agua = contador_culturas * 3;
+    regen_comida = contador_culturas * 1;
+    regen_sementes = contador_culturas * 1;
+}
+
+void recursos_quinta(void)
+{
+    // atualizar recursos com consumos e regenerações (comida, água, sementes)
+    printf("A actualizar os recursos da quinta...\n");
+    usleep(1000000);
+    comida += regen_comida;
+    sementes += regen_sementes;
+    agua += regen_agua - consumo_agua;
+    if (agua >= capacidade_agua)
+    {
+        agua = capacidade_agua; // depósito cheio
+    }
+}
+
+int evento_intemperie(void)
+{
+    // Gera um número entre 1 e 100
+    int evento = (rand() % 100) + 1;
+
+    if (evento <= 50)
+    {
+        return 0; // 50%
+    }
+    else if (evento <= 70)
+    {
+        return 1; // 20%; vai ser usada para tempestade
+    }
+    else if (evento <= 80)
+    {
+        return 2; // 10%; vai ser usada para seca
+    }
+    else if (evento <= 90)
+    {
+        return 3; // 10%; vai ser usado para pássaros
+    }
+    else
+    {
+        return 4; // 10%; furo no depósito
+    }
+}
+
+void crescer_sementes(char quinta[3][3]) // ver o que têm os hectares de terreno
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (quinta[i][j] == 's')
+            {
+                quinta[i][j] = 'C';
+            }
+        }
+    }
+}
+
+void passaros(void)
+{
+    printf("Um bando de pássaros atacou o celeiro! Perdeste metade das sementes.\n");
+    sementes /= 2;
 }
